5a4dcdb45746fde1f4d8094c107bb6da7b301274
tandisunarto <tandi.sunarto@hotmail.com>
2016-07-22 16:40:45 -0400

    Initial Checkin

---
+++ OwinKatana/KatanaIntro/Startup.cs
@@ -0,0 +1,27 @@
+﻿using KatanaIntro.Components;
+using KatanaIntro.Extensions;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaIntro
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.Use<HelloWorldComponent>();
+            app.UseHelloWorld();
+
+            //app.UseWelcomePage();
+
+            //app.Run(ctx =>
+            //{
+            //    return ctx.Response.WriteAsync("Hello World 2016 !");
+            //});
+        }
+    }
+}
