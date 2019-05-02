4137217c703e80f17de1840c19e742ece4cbefcf
Tandi Sunarto <tandi.sunarto@hotmail.com>
2019-05-02 11:12:08 -0400

    Update nuget packages

--- KatanaIntro/Controllers/GreetingController.cs
+++ KatanaIntro/Controllers/GreetingController.cs
@@ -1,4 +1,4 @@
-﻿using KatanaIntroIIS.ViewModels;
+﻿using KatanaIntro.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 
-namespace KatanaIntroIIS.Controllers
+namespace KatanaIntro.Controllers
 {
     public class GreetingController : ApiController
     {
--- KatanaIntro/ViewModels/GreetingViewModel.cs
+++ KatanaIntro/ViewModels/GreetingViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KatanaIntroIIS.ViewModels
+namespace KatanaIntro.ViewModels
 {
     public class GreetingViewModel
     {
